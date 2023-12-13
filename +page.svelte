0647c82837b6ac19a298287c83b10ce95a3283b4
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2023-12-13 04:13:47 +0330

    Create +page.svelte

---
+++ +page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {Store} from '../store.svelte'
+	import Child from './child.svelte';
+	let store = new Store()
+
+</script>
+
+<button on:click={() => store.count++}>
+	clicks: {store.count}
+</button>
+<Child store="{store}"/>
